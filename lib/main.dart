9aa553d55d30ab92fdf9d588a53aa50a93bd8f4e
Genesis Jr <72869897+Sliceofbanana@users.noreply.github.com>
2024-06-26 17:07:39 +0800

    updated, real-time, burger, SOS button, sliding up panel

--- lib/main.dart
+++ lib/main.dart
@@ -28,22 +28,31 @@ class MapScreen extends StatefulWidget {
 class _MapScreenState extends State<MapScreen> {
   static const _initialCameraPosition = CameraPosition(
     target: LatLng(11.083018, 123.931450),
-    zoom: 11.5,
+    zoom: 13.5,
   );
 
   late GoogleMapController _googleMapController;
   final PanelController _panelController = PanelController();
+  final ValueNotifier<double> _fabPositionNotifier =
+      ValueNotifier<double>(16.0);
 
   @override
   void dispose() {
     _googleMapController.dispose();
+    _fabPositionNotifier.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    final fabHeightClosed = screenHeight - 735;
+    final fabHeightOpened = screenHeight - 290;
+
     return Scaffold(
-      /*body: GoogleMap(
+      //Google Map only walay sliding up panel
+      /*
+      body: GoogleMap(
         myLocationButtonEnabled: false,
         zoomControlsEnabled: false,
         initialCameraPosition: _initialCameraPosition,
@@ -59,23 +68,146 @@ class _MapScreenState extends State<MapScreen> {
       ),
     );
   }*/
-      body: SlidingUpPanel(
-        controller: _panelController,
-        panelBuilder: (controller) => PanelWidget(controller: controller),
-        body: GoogleMap(
-          myLocationButtonEnabled: false,
-          zoomControlsEnabled: false,
-          initialCameraPosition: _initialCameraPosition,
-          onMapCreated: (controller) => _googleMapController = controller,
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.red[700],
+              ),
+              child: Text(
+                'Menu',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.info),
+              title: Text('About Us'),
+              onTap: () {
+                // Handle About Us tap
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.feedback),
+              title: Text('Feedback'),
+              onTap: () {
+                // Handle Feedback tap
+                Navigator.pop(context);
+              },
+            ),
+          ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Theme.of(context).primaryColor,
-        foregroundColor: Colors.black,
-        onPressed: () => _googleMapController.animateCamera(
-          CameraUpdate.newCameraPosition(_initialCameraPosition),
-        ),
-        child: const Icon(Icons.center_focus_strong),
+      body: Stack(
+        children: [
+          GoogleMap(
+            myLocationButtonEnabled: false,
+            zoomControlsEnabled: false,
+            initialCameraPosition: _initialCameraPosition,
+            onMapCreated: (controller) => _googleMapController = controller,
+          ),
+          SlidingUpPanel(
+            controller: _panelController,
+            panelBuilder: (controller) => PanelWidget(controller: controller),
+            backdropEnabled: true,
+            backdropColor: Colors.transparent,
+            color: Colors.transparent,
+            minHeight: 125,
+            maxHeight: screenHeight * 0.64,
+            margin: EdgeInsets.only(
+                bottom: 98), // Adjust margin to make space for the button
+            onPanelSlide: (position) {
+              _fabPositionNotifier.value = fabHeightClosed -
+                  ((fabHeightClosed - fabHeightOpened) * position);
+            },
+          ),
+          Positioned(
+            left: 16,
+            top: 30,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 10,
+                    offset: Offset(0, -5),
+                  ),
+                ],
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Builder(
+                builder: (context) {
+                  return IconButton(
+                    icon: Icon(Icons.menu, color: Colors.black),
+                    onPressed: () {
+                      Scaffold.of(context).openDrawer();
+                    },
+                  );
+                },
+              ),
+            ),
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: Container(
+              decoration: BoxDecoration(
+                color:
+                    Colors.white, // White background for the button container
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 10,
+                    offset: Offset(0, -5),
+                  ),
+                ],
+              ),
+              padding: EdgeInsets.symmetric(vertical: 16),
+              child: Center(
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.red,
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 100, vertical: 20),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  child: Text(
+                    "EMERGENCY ALERT",
+                    style: TextStyle(fontSize: 18, color: Colors.white),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          ValueListenableBuilder<double>(
+            valueListenable: _fabPositionNotifier,
+            builder: (context, value, child) {
+              return Positioned(
+                right: 16,
+                bottom:
+                    value + 80, // Adjust position to account for button height
+                child: FloatingActionButton(
+                  backgroundColor: Theme.of(context).primaryColor,
+                  foregroundColor: Colors.black,
+                  onPressed: () => _googleMapController.animateCamera(
+                    CameraUpdate.newCameraPosition(_initialCameraPosition),
+                  ),
+                  child: const Icon(Icons.center_focus_strong),
+                ),
+              );
+            },
+          ),
+        ],
       ),
     );
   }
--- lib/panel_widget.dart
+++ lib/panel_widget.dart
@@ -1,73 +1,139 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-class PanelWidget extends StatelessWidget {
+class PanelWidget extends StatefulWidget {
   final ScrollController controller;
 
   const PanelWidget({Key? key, required this.controller}) : super(key: key);
 
   @override
+  _PanelWidgetState createState() => _PanelWidgetState();
+}
+
+class _PanelWidgetState extends State<PanelWidget> {
+  late Timer _timer;
+  late DateTime _currentTime;
+
+  @override
+  void initState() {
+    super.initState();
+    _currentTime = DateTime.now();
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {
+        _currentTime = DateTime.now();
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final now = DateTime.now();
-    final formattedDate = DateFormat('yyyy-MM-dd – kk:mm').format(now);
+    final formattedDate =
+        DateFormat('yyyy-MM-dd – hh:mm:ss a').format(_currentTime);
 
     return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            children: [
-              Text("Date and Time: "),
-              Text(formattedDate, style: TextStyle(color: Colors.black)),
-            ],
-          ),
-          SizedBox(height: 10),
-          Row(
-            children: [
-              Text("Weather Conditions: "),
-              Text("RED ALERT",
-                  style: TextStyle(
-                      color: Colors.red, fontWeight: FontWeight.bold)),
-              Icon(Icons.warning, color: Colors.red),
-            ],
-          ),
-          SizedBox(height: 20),
-          Expanded(
-            child: GridView.count(
-              crossAxisCount: 2,
+      padding: const EdgeInsets.all(0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.transparent, // Outer container is transparent
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(0), // Remove inner padding
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white, // Inner panel background
+              borderRadius: BorderRadius.vertical(top: Radius.circular(30)),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                _buildWeatherCard("Temperature", "40%"),
-                _buildWeatherCard("Wave Speed", "60%"),
-                _buildWeatherCard("Humidity", "27%"),
-                _buildWeatherCard("Rain Intensity", "60%"),
+                // Handle Indicator
+                Center(
+                  child: Container(
+                    width: 60,
+                    height: 5,
+                    decoration: BoxDecoration(
+                      color: Colors.grey,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    margin: const EdgeInsets.only(top: 15, bottom: 25),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 20.0), // Adjust padding
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Icon(Icons.calendar_today, color: Colors.red[700]),
+                          SizedBox(width: 8),
+                          Text("Date and Time: ",
+                              style: TextStyle(fontSize: 16)),
+                          Text(formattedDate,
+                              style:
+                                  TextStyle(fontSize: 16, color: Colors.black)),
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                      Row(
+                        children: [
+                          Icon(Icons.cloud, color: Colors.red[700]),
+                          SizedBox(width: 8),
+                          Text("Weather Conditions: ",
+                              style: TextStyle(fontSize: 16)),
+                          Text("RED ALERT",
+                              style: TextStyle(
+                                  color: Colors.red,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold)),
+                          Icon(Icons.warning, color: Colors.red),
+                        ],
+                      ),
+                      SizedBox(height: 20),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 10.0, vertical: 15.0), // Adjust padding
+                    child: GridView.count(
+                      crossAxisCount: 2,
+                      padding: EdgeInsets.all(0),
+                      children: [
+                        _buildWeatherCard("Temperature", "40%"),
+                        _buildWeatherCard("Wave Speed", "60%"),
+                        _buildWeatherCard("Humidity", "27%"),
+                        _buildWeatherCard("Rain Intensity", "60%"),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(height: 5), // Additional padding at the bottom
               ],
             ),
           ),
-          SizedBox(height: 20),
-          Center(
-            child: ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.red,
-                padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
-              ),
-              child: Text(
-                "EMERGENCY ALERT",
-                style: TextStyle(fontSize: 18, color: Colors.white),
-              ),
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
 
   Widget _buildWeatherCard(String title, String value) {
     return Card(
-      elevation: 4,
+      elevation: 0, // Remove the shadow
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20), // Rounded corners
+      ),
       child: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
